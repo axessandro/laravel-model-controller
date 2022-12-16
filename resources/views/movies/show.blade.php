e8d481b041ef36a95d392226ed4f92cbe12de0e7
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-12-16 18:14:43 +0100

    Added details on single movie

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,6 +4,22 @@
     <div class="container">
         <button class="btn btn-danger"><a href="/">Home</a></button>
         <button class="btn btn-danger"><a href="/movies">List</a></button>
-        <h1>{{ $movies[$index]->title }}</h1>
+
+        <div class="container">
+
+            <div class="ms-card">
+                <div class="img-wrapper">
+                    <img src="{{ $movies[$index]->image }}" alt="">
+                </div>
+
+                <div class="text-wrapper">
+                    <h1>{{ $movies[$index]->title }}</h1>
+                    <h3>{{ $movies[$index]->original_title }}</h3>
+                    <h5>{{ $movies[$index]->date }}</h5>
+                    <h5>Vote: {{ $movies[$index]->vote }}</h5>
+
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
